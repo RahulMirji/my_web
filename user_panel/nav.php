cbe842a4d68f021c2a437031e0ce4f15b34b18af
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-08-15 23:15:06 +0530

    commited

--- user_panel/nav.php
+++ user_panel/nav.php
@@ -13,7 +13,7 @@
             box-sizing: border-box;
         }
 
-        /* Body and Container full */
+        /* Body and Container  */
         body {
             font-family: 'Arial', sans-serif;
             background-color: #f4f4f4;
