df6af9a3e37b7f6ca1e07fc6566e4d2e74e66f40
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-08-21 22:00:20 +0530

    commited

--- user_panel/contact.php
+++ user_panel/contact.php
@@ -1,5 +1,5 @@
 <?php
-include('../db.php'); // Ensure this file connects to your database correctly
+include('../db.php'); // Ensure this file connects to your database correct
 
 if (isset($_POST['send'])) {
     // Declare all variables
