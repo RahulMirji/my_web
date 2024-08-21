98c7ec012e96287099f89eff2219900dba2c1849
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-08-21 21:54:53 +0530

    commited

--- user_panel/contact.php
+++ user_panel/contact.php
@@ -35,7 +35,7 @@ if (isset($_POST['send'])) {
     ?>
     <link rel="stylesheet" href="styles.css">
     <style>
-        /* General Styles */
+        /* General St
 body {
     font-family: Arial, sans-serif;
     line-height: 1.6;
