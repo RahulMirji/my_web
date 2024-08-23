45ad07423a4fba7ccc9995ab18214e69d4fd422b
RahulMirji <121561908+RahulMirji@users.noreply.github.com>
2024-08-23 20:05:44 +0530

    updated contact

--- user_panel/contact.php
+++ user_panel/contact.php
@@ -35,7 +35,7 @@ if (isset($_POST['send'])) {
     ?>
     <link rel="stylesheet" href="styles.css">
     <style>
-        /* General Styles 
+        /* General Style
 body {
     font-family: Arial, sans-serif;
     line-height: 1.6;
